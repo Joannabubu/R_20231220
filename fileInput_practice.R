6c1dbf0825cb43c8fd4c49eb0cb2218b69c41429
le402n39\uer <409402570@m365.fju.edu.tw>
2023-12-20 16:21:52 +0800

    practice upload file

---
+++ fileInput_practice.R
@@ -0,0 +1,9 @@
+#撰寫一個 APP，當使用者賞船檔案時，就能看到他所上傳的資料之變動
+
+Data=read.table("Example.txt", header = TRUE)
+#讀取Example.txt，並存成"Data"這個物件(資料表格式)
+head(Data, 10) 
+#看"Data"這個資料表的前10個row
+summary(Data)
+#看"Data"這個資料表的所有變項的基本資訊
+

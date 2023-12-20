5c7f3eea75ec4d1b2de6f20bad690d2457d6a472
le402n39\uer <409402570@m365.fju.edu.tw>
2023-12-20 17:06:51 +0800

    新增  Pearson correlation & Spearman correlation 的程式

--- control_server.R
+++ control_server.R
@@ -17,4 +17,15 @@ colnames(Data)[4]
 Result1 = lm(Data[,"Weight"]~Data[,"Height"])
 Result1
 summary(Result1)
-#導入 renderUI 及 utOutput 兩個函數，它們能讓我們先讀取檔案，再讓控制區出現檔案內的變數
+#導入 renderUI 及 uiOutput 兩個函數，它們能讓我們先讀取檔案，再讓控制區出現檔案內的變數
+#renderUI : 動態UI 輸入
+# uiOutput : 動態 UI 輸出
+
+
+# Pearson correlation
+Result2 = cor.test(Data[,"Weight"], Data[,"Height"], method = "pearson") 
+Result2
+
+# Spearman correlation
+Result3 = cor.test(Data[,"Weight"], Data[,"Height"], method = "spearman") 
+Result3
